0875b06cb3e5295f0370c7cca06ead3329983c5e
tenvinc <vencintgamer@gmail.com>
2020-05-24 12:17:08 +0800

    Implemented gait for turning and walking forward

--- esp8266_robot/main.cpp
+++ esp8266_robot/main.cpp
@@ -11,7 +11,8 @@ extern "C" {
 #include <string.h>
 #include <task.h>
 }
-#include "PCA9685_driver.h"
+#include <algorithm>
+#include "hexapod.h"
 
 #define LED_PIN 2
 
@@ -20,69 +21,15 @@ extern "C" {
 
 #define SYS_PRINTF(fmt, ...) printf("%s: " fmt, "MAIN", ##__VA_ARGS__)
 
-PCA9685_Servo_Driver driver;
-
-typedef struct servo_intrinsics {
-  uint16_t min_pw;      // min pulse width
-  uint16_t max_pw;      // max pulse width
-  uint8_t angle_range;  // corresponding angle range with 0 degrees when pulse
-                        // width = min_pw
-} servo_intrinsics_t;
-
-servo_intrinsics_t SERVO_INTRINSICS_LUT[12] = {
-    {600, 2620, 180}, {600, 2620, 170}, {600, 2650, 180}, {600, 2650, 175},
-    {600, 2650, 175}, {600, 2650, 175}, {700, 2650, 180}, {700, 2650, 170},
-    {700, 2650, 175}, {700, 2750, 180}, {600, 2550, 180}, {500, 2550, 180},
-};
-
-bool SERVO_INVRT_LUT[12] = {    // indicates which servo is inverted
-  false, false, 
-  false, false, 
-  true, false,
-  false, false,
-  false, false,
-  false, false
-};
+Hexapod hexy;
 
 void sensor_task(void *pvParameters) {
-  driver.begin();
-  for (int i = 0; i < 12; i++) {
-    driver.attach(i, SERVO_INTRINSICS_LUT[i].min_pw,
-                  SERVO_INTRINSICS_LUT[i].max_pw,
-                  SERVO_INTRINSICS_LUT[i].angle_range);
-    if (i%2) {
-      driver.writeAngle(i, 75);
-    } else {
-      driver.writeAngle(i, SERVO_INVRT_LUT[i] ? SERVO_INTRINSICS_LUT[i].angle_range - 0 : 0);
-    }
+  hexy.init();
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  for (int i=0; i<10; i++) {
+    hexy.walkForward();
+    vTaskDelay(200 / portTICK_PERIOD_MS);
   }
-  // while (1) {
-  //   for (int ms = min; ms <= max; ms+=10) {
-  //     driver.writeMicroseconds(1, ms);
-  //     vTaskDelay(5 / portTICK_PERIOD_MS);
-  //   }
-  //   for (int ms = max; ms >= min; ms-=10) {
-  //     driver.writeMicroseconds(1, ms);
-  //     vTaskDelay(5 / portTICK_PERIOD_MS);
-  //   }
-  // }
-  // while (1) {
-  //   for (int n = 0; n < 11; n++) {
-  //     SYS_PRINTF("Adjusting servo %d\n", n);
-  //     for (int i=75; i>0; i--) {
-  //       driver.writeAngle(n, i);
-  //       vTaskDelay(5 / portTICK_PERIOD_MS);
-  //     }
-  //     for (int i = 0; i < 150; i++) {
-  //       driver.writeAngle(n, i);
-  //       vTaskDelay(5 / portTICK_PERIOD_MS);
-  //     }
-  //     for (int i=150; i>=75; i--) {
-  //       driver.writeAngle(n, i);
-  //       vTaskDelay(5 / portTICK_PERIOD_MS);
-  //     }
-  //   }
-  // }
   vTaskDelete(NULL);
 }
 
